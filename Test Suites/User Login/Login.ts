f7f46df0224f6ca099bb20049dfc81c8032282eb
ibnufajaryunardi <ibnufajaryunardi@localhost>
2022-10-13 20:17:52 +0700

    Add Cucumber Feature

---
+++ Test Suites/User Login/Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>120</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>01b3c3be-2ace-4271-9ddf-74822c8af0b5</testSuiteGuid>
+   <testCaseLink>
+      <guid>eed24bb6-7859-42c3-8201-9d3721f9f60a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pages/StepDefinition/LG001-Login With Valid Username</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
